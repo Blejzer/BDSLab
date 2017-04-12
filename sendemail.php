dd00f100cbcf5581bbae20485b012b29a4817157
Blejzer <nikola.kujaca@gmail.com>
2017-04-12 14:21:57 +0200

    smanjio slike i pokusao popraviti sendmail bezuspjesno

--- sendemail.php
+++ sendemail.php
@@ -3,16 +3,16 @@ $name       = @trim(stripslashes($_POST['name']));
 $from       = @trim(stripslashes($_POST['email'])); 
 $subject    = @trim(stripslashes($_POST['subject'])); 
 $message    = @trim(stripslashes($_POST['message'])); 
-$to   		= 'nikola.kujaca@gmail.com';
+$to   		= 'nikola.kujaca@email.com';//replace with your email
 
 $headers   = array();
-$headers[] = "MIME-Version: 1.0";
-$headers[] = "Content-type: text/plain; charset=iso-8859-1";
-$headers[] = "From: {$name} <{$from}>";
+$headers  .= "MIME-Version: 1.0";
+$headers  .= "Content-type: text/plain; charset=iso-8859-1";
+$headers  .= "From: {$name} <{$from}>";
 $headers[] = "Reply-To: <{$from}>";
 $headers[] = "Subject: {$subject}";
 $headers[] = "X-Mailer: PHP/".phpversion();
 
-sendmail($to, $subject, $message, $headers);
+mail($to, $subject, $message, $headers);
 
-die;
+¸¸die;
