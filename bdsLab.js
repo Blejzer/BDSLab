d29ee4745da7123e75e2ddfd9d876cc6ffa7b51d
Blejzer <nikola.kujaca@gmail.com>
2026-02-10 09:58:14 +0100

    From scratch

--- bdsLab.js
+++
@@ -1,7 +0,0 @@
-/*******************************************************
- * Copyright (C) Nikola Kujaca - All Rights Reserved    *
- * Unauthorized copying of this file, via any medium    *
- * is strictly prohibited proprietary and confidential  *
- * Written by Nikola Kujaca <nikola.kujaca@gmail.com>,  *
- * 1411972                                              *
- ********************************************************/
